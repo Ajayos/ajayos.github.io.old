d223c7df84a4a8aa53017878f58d56008487dbc4
Ajay o s <ajayosakhub@gmail.com>
2023-06-25 21:50:22 +0530

    update set

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 import reportWebVitals from "./reportWebVitals";
-import HelmetProvider_ from "./contexts/HelmetContext";
-import AuthProvider from "./contexts/AuthContext";
-import SocketProvider from "./contexts/SocketContext";
-import ThemeModelProvider from "./contexts/ThemeModeContext";
+// import HelmetProvider_ from "./contexts/HelmetContext";
+// import AuthProvider from "./contexts/AuthContext";
+// import SocketProvider from "./contexts/SocketContext";
+// import ThemeModelProvider from "./contexts/ThemeModeContext";
 
 import Router from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -16,20 +16,20 @@ root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<HelmetProvider>
-				<HelmetProvider_>
-					<SocketProvider>
-						<AuthProvider>
-							<ThemeModelProvider>
+				{/* <HelmetProvider_> */}
+					{/* <SocketProvider> */}
+						{/* <AuthProvider> */}
+							{/* <ThemeModelProvider> */}
 								<SnackbarProvider
 									maxSnack={3}
 									anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
 								>
 									<Router />
 								</SnackbarProvider>
-							</ThemeModelProvider>
-						</AuthProvider>
-					</SocketProvider>
-				</HelmetProvider_>
+							{/* </ThemeModelProvider> */}
+						{/* </AuthProvider> */}
+					{/* </SocketProvider> */}
+				{/* </HelmetProvider_> */}
 			</HelmetProvider>
 		</BrowserRouter>
 	</React.StrictMode>
