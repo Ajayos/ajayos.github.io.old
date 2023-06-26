f495488b01a4c4cdedbc86458f57a573483e4a2d
Ajay o s <ajayosakhub@gmail.com>
2023-06-26 15:29:10 +0000

    👨‍💻CODE BY 🕊️★⃝AJAY O S©️🧚‍♂️

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter } from "react-router-dom";
-import { SnackbarProvider } from "notistack";
 import reportWebVitals from "./reportWebVitals";
 
 import Router from "./App";
