6d268bcc5d533156b5a02dd40dfdf08993424e77
Ajay o s <ajayosakhub@gmail.com>
2023-06-26 07:32:27 +0530

    starting page building

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello </h1>
-      <h2>{window.backendUrl}!</h2>
+      <h2>PAGE UNDER MAINTENANCE</h2>
     </div>
   );
 }
